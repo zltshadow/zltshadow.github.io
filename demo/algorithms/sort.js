ebf23c3d7f7f00759cb41d08898e103ae4c9cf5d
zltshadow <zlt_shadow@163.com>
2021-04-13 17:56:42 +0800

    JS实现快排的两种写法，一种简洁，一种递归

---
+++ demo/algorithms/sort.js
@@ -0,0 +1,69 @@
+// 快排简洁写法
+var quickSort = function (arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  var pivotIndex = Math.floor(arr.length / 2);
+  var pivot = arr.splice(pivotIndex, 1)[0];
+  var left = [];
+  var right = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat([pivot], quickSort(right));
+};
+
+
+// 快排递归写法
+function swap(A, i, j) {
+  const t = A[i];
+  A[i] = A[j];
+  A[j] = t;
+  // 交换还可以使用[A[i],A[j]] = [A[j],A[i]];
+}
+
+/**
+ * @name 划分
+ * @param {*} A  数组
+ * @param {*} p  起始下标
+ * @param {*} r  结束下标 + 1
+ */
+function divide(A, p, r) {
+  const x = A[r - 1];
+  let i = p - 1;
+
+  for (let j = p; j < r - 1; j++) {
+    if (A[j] <= x) {
+      i++;
+      swap(A, i, j);
+    }
+  }
+
+  swap(A, i + 1, r - 1);
+
+  return i + 1;
+}
+
+/**
+ * 
+ * @param {*} A  数组
+ * @param {*} p  起始下标
+ * @param {*} r  结束下标 + 1
+ */
+function qsort(A, p = 0, r) {
+  r = r || A.length;
+
+  if (p < r - 1) {
+    const q = divide(A, p, r);
+    qsort(A, p, q);
+    qsort(A, q + 1, r);
+  }
+
+  return A;
+}
+console.log(qsort([1, 3, 5, 7, 2]));
